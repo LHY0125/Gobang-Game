8dfab7155936a6288820807354278933f13223f9
LHY20 <3364451258@qq.com>
2025-07-20 00:46:05 +0800

    更新main.c文件

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 /**
  * @brief 将指令复制到powershell
- * gcc -o gobang.exe *.c -lws2_32 
+ * gcc -std=c17 -o gobang.exe *.c -lws2_32
    .\gobang.exe
  * gcc 为编译器，添加了network.c网络模块，-lws2_32链接Windows网络库
  */
